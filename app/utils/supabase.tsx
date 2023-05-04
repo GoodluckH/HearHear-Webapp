82ee4d0550ebb45bbb5e67e40dea25273dca2ab3
Xipu Li <lixipu98@gmail.com>
2023-05-03 21:09:24 -0700

    bump free credits to 500

--- app/utils/supabase.tsx
+++ app/utils/supabase.tsx
@@ -45,7 +45,7 @@ export class MySupabaseClient {
             id: user.id,
             display_name: user.displayName,
             discriminator: user.discriminator,
-            credits: 100,
+            credits: 500,
           },
         ]);
       }
